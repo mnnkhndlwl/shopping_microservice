288150c4b4dd36cf31e297479a0b8b094ceb8d3d
mnnkhndlwl <khandelwalmanan05@gmail.com>
2023-07-29 13:47:26 +0530

    new feature

--- shopping/src/services/shopping-service.test.js
+++ shopping/src/services/shopping-service.test.js
@@ -1,6 +1,6 @@
 // which service it is
 describe("ShoppingService", () => {
-    // Which function
+    // Which function we are testing
     describe("PlaceOrder", () => {
       // Which Scenario we are testing
       test("validate user inputs", () => {});
