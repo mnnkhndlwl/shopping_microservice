370f6b141ada82de2d5752ffd9b3e231fd742ef4
mnnkhndlwl <khandelwalmanan05@gmail.com>
2023-09-10 00:59:12 +0530

    commit

---
+++ products/src/database/repository/product-repository.js
@@ -0,0 +1,77 @@
+const { ProductModel } = require("../models");
+const { APIError, BadRequestError } = require("../../utils/app-errors");
+
+//Dealing with data base operations
+class ProductRepository {
+  async CreateProduct(
+    data
+  ) {
+    try {
+      const product = new ProductModel({...data});
+
+      const productResult = await product.save();
+      return productResult;
+    } catch (err) {
+      throw new APIError(
+        "API Error",
+        STATUS_CODES.INTERNAL_ERROR,
+        "Unable to Create Product"
+      );
+    }
+  }
+
+  async Products() {
+    try {
+      return await ProductModel.find();
+    } catch (err) {
+      throw new APIError(
+        "API Error",
+        STATUS_CODES.INTERNAL_ERROR,
+        "Unable to Get Products"
+      );
+    }
+  }
+
+  async FindById(id) {
+    try {
+      return await ProductModel.findById(id);
+    } catch (err) {
+      throw new APIError(
+        "API Error",
+        STATUS_CODES.INTERNAL_ERROR,
+        "Unable to Find Product"
+      );
+    }
+  }
+
+  async FindByCategory(category) {
+    try {
+      const products = await ProductModel.find({ category: category });
+      return products;
+    } catch (err) {
+      throw new APIError(
+        "API Error",
+        STATUS_CODES.INTERNAL_ERROR,
+        "Unable to Find Category"
+      );
+    }
+  }
+
+  async FindSelectedProducts(selectedIds) {
+    try {
+      const products = await ProductModel.find()
+        .where("_id")
+        .in(selectedIds.map((_id) => _id))
+        .exec();
+      return products;
+    } catch (err) {
+      throw new APIError(
+        "API Error",
+        STATUS_CODES.INTERNAL_ERROR,
+        "Unable to Find Product"
+      );
+    }
+  }
+}
+
+module.exports = ProductRepository;
---
+++ products/src/express-app.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const cors  = require('cors');
+const { products, appEvents } = require('./api');
+
+
+module.exports = async (app,channel) => {
+
+    app.use(express.json({ limit: '2mb'}));
+   // app.use(express.urlencoded({ extended: true, limit: '1mb'}));
+    app.use(cors());
+    app.use(express.static(__dirname + '/public'))
+
+    //handle events
+   // appEvents(app);
+
+    //api
+    products(app,channel);
+
+    // error handling
+   // app.use(HandleErrors);
+    
+}
